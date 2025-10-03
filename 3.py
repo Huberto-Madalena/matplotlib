e6f20ea65ebf70a2d67929d75817c7a181c0f0a3
mark-ganus <mark_volny@yahoo.com.au>
2025-10-03 00:55:49 -0300

    commiting next files for plot design

---
+++ 3.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#simple squared chart
+x_1 = np.linspace(0,5,10)
+#np.linspace takes the values of the x axle and uses them as reference for future calculations
+y_1 = x_1**2;
+#the values for the y axle on our chart will have the values of x squared
+#this is also a quadratic function,as the line follows an exponential growth rate
+plt.plot(x_1, y_1)
+plt.title('days squared chart')
+plt.xlabel('days')
+plt.ylabel('days squared')
+
+#splitting the chart up
+plt.subplot(1,2,1)
+plt.plot(x_1,y_1, 'r')
+plt.subplot(1,2,2)
+plt.plot(x_1,y_1,'b')
+
+plt.show()
---
+++ 4.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#simple squared chart
+x_1 = np.linspace(0,5,10)
+#np.linspace takes the values of the x axle and uses them as reference for future calculations
+y_1 = x_1**2;
+#the values for the y axle on our chart will have the values of x squared
+#this is also a quadratic function,as the line follows an exponential growth rate
+plt.plot(x_1, y_1)
+plt.title('days squared chart')
+plt.xlabel('days')
+plt.ylabel('days squared')
+
+#splitting the chart up
+plt.subplot(1,2,1)
+plt.plot(x_1,y_1, 'r')
+#heads up! the 3rd parameter is the colour scheme for  teh chart (r for red, b for blue etc)
+plt.subplot(1,2,2)
+plt.plot(x_1,y_1,'b')
+
+
+fig_1 = plt.figure(figsize=(5,4), dpi=100)
+axes_1 = fig_1.add_axes([0.1,0.1,0.9,0.9])
+axes_1.set_xlabel('days')
+axes_1.set_ylabel('days squared')
+axes_1.set_title('days squared chart')
+axes_1.plot(x_1,y_1,label='x/x²')
+axes_1.plot(y_1,y_1,label='x²/x')
+axes_1.legend(loc=0)
+
+
+plt.show()
